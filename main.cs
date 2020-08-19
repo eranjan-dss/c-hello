75cc13b33d33f8f0fe9cd42c18d513a32bfa1c49
eranjan <sunethra8023@gmail.com>
2020-08-19 05:30:19 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class MainClass
+{
+  public static void Main (string[] args)
+  {
+    string stono = Console.ReadLine();
+    if(stono == "start")
+    {
+      Console.WriteLine("hello world");
+      Console.ReadLine();
+    }
+  }
+}
